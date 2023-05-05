022b4daf746ca22bb2b6fa87aef34feb1ee39cf1
Shuaib Nuruddin <95973048+ShuaibNuruddin@users.noreply.github.com>
2023-05-05 15:01:37 +0200

    Add files via upload

---
+++ functions/src/index.ts
@@ -0,0 +1,10 @@
+const functions = require('firebase-functions');
+//import * as functions from "firebase-functions";
+
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
