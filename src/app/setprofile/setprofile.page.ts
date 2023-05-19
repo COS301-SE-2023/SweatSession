583dcd0215e6c69d824fdf15b4b2ed0adf0f0ee9
Triumph Ndlovu <realtriumphndlovu@gmail.com>
2023-05-20 01:56:32 +0200

    Edit Profile(Some front End)

--- src/app/setprofile/setprofile.page.ts
+++ src/app/setprofile/setprofile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { profile } from 'console';
 
 @Component({
   selector: 'app-setprofile',
@@ -7,6 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetprofilePage implements OnInit {
 
+  user: any = {
+    profile: 'https://i.pravatar.cc/150?img=68',
+    name: 'Triumph Ndlovu',
+    email: 'TriumphSynapse@gmail.com',
+    bio: 'Hii there, Im a Natty',
+  };
+  oldProfilePicture = this.user.profile
+  selectedPicture: string | null = null;
+
+  onPictureChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.selectedPicture = reader.result as string;
+
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  saveProfile() {
+ 
+    this.user.profile = this.selectedPicture;
+    console.log('Saving profile:', this.user);
+  }
   constructor() { }
 
   ngOnInit() {
