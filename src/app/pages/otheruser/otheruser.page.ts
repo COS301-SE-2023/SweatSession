e29b211ca00e491654ae65450d1b01a90bf7368f
ThaboLetsoalo <u20520222@tuks.co.za>
2023-05-20 03:00:56 +0200

    modified repo structure. Added state, services, repository and model folders to contain their respective files.

---
+++ src/app/pages/otheruser/otheruser.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-otheruser',
+  templateUrl: './otheruser.page.html',
+  styleUrls: ['./otheruser.page.scss'],
+})
+export class OtheruserPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
