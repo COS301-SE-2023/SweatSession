47f125941ee186132968d47bc683c46a76fac7c8
ThaboLetsoalo <u20520222@tuks.co.za>
2023-05-31 11:39:54 +0200

    workout schedule modified.
    User can be able to search schedules, add them and view them

--- src/app/pages/workout-scheduling/workout-scheduling.module.ts
+++ src/app/pages/workout-scheduling/workout-scheduling.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { WorkoutSchedulingPage } from './workout-scheduling.page';
 import { WorkoutSchedulingPageRoutingModule } from './workout-scheduling-routing.module';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { PopoutScheduleComponent } from './popout-schedule/popout-schedule.component';
 import { PopoutAddScheduleComponent } from "./popout-add-schedule/popout-add-schedule.component";
+import { WorkoutSchedulingState, WorkoutSchedulingStateModule } from 'src/app/states';
+import { NgxsModule } from '@ngxs/store';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    WorkoutSchedulingPageRoutingModule
+    WorkoutSchedulingPageRoutingModule,
+    WorkoutSchedulingStateModule
   ],
   declarations: [WorkoutSchedulingPage,ScheduleComponent,PopoutScheduleComponent, PopoutAddScheduleComponent]
 })
