2e7c02c8021cb11763bb61b2223c419411be8d6f
LUQMAAN-ABRAM <104630753+LUQMAAN-ABRAM@users.noreply.github.com>
2023-06-02 22:53:35 +0200

    Added To Notifications

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import {NoticeService } from 'src/app/services/notifications/notice.service';
 import { Notice } from 'src/app/models/notice.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-notifications',
@@ -12,19 +13,47 @@ import { Notice } from 'src/app/models/notice.model';
 
 
 export class NotificationsPage implements OnInit {
-
-  toggleswitch = true ;
-
-  
-  noticeList: Observable<Notice[]> = this.noticeService.getNotices();
   
 
-  constructor(private noticeService: NoticeService) { }
-
-  ngOnInit() {}
+  noticeamount : number ;
+  noticeList: Notice[];
 
   
+  //noticeList: Observable<Notice[]> = this.noticeService.getNotices();
+ 
 
+  constructor(private noticeService: NoticeService , private alertController: AlertController) { 
+    
+  }
+ 
+  ngOnInit() {
+    this.getNotifications() ;
+      
+  }
+
+  getNotifications(){
+    this.noticeService.getNotices().subscribe((notices: Notice[]) => {
+      this.noticeList = notices;
+      console.log('Number of notices:', this.noticeList.length);
+      this.noticeamount = this.noticeList.length ;
+      console.log(this.noticeamount);
+    });  
+
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message: 'You have no new notifications!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  clearNotifications(){
+    this.noticeList = [] ;
+  }
   
 
 
