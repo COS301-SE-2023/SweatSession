8d547b293d109a97a4d8e2acd090b38669c0a3d6
ShuaibNuruddin <u21497402@tuks.co.za>
2023-06-07 00:24:52 +0200

    Made Badges UI and added it to home routing

--- src/app/pages/badges/badges.page.ts
+++ src/app/pages/badges/badges.page.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./badges.page.scss'],
 })
 export class BadgesPage implements OnInit {
-
+  badges=[{
+      header:"Starter's Success",
+      description:"Congratulations! You have completed your first workout plan",
+      received:true
+    },{
+      header:"Starter's Success",
+      description:"Congratulations! You have completed your first workout plan",
+      received:true
+    },{
+      header:"Starter's Success",
+      description:"Congratulations! You have completed your first workout plan",
+      received:false
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
