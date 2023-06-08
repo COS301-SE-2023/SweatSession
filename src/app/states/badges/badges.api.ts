5f2addf07c7b81075d0e891eecc415f2de57a14b
Shuaib Nuruddin <95973048+ShuaibNuruddin@users.noreply.github.com>
2023-06-09 00:27:46 +0200

    can get from firebase and display in the UI

--- src/app/states/badges/badges.api.ts
+++ src/app/states/badges/badges.api.ts
@@ -3,35 +3,25 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { doc, docData, Firestore } from '@angular/fire/firestore';
 import { IBadges } from 'src/app/models/badges.model';
 import { BadgesRepository } from 'src/app/repository/badges.repository'
-// import {
-//   Auth,
-//   authState,
-//   createUserWithEmailAndPassword,
-//   signInWithEmailAndPassword,
-//   getAuth
-// } from '@angular/fire/auth';
 
-// import { signOut } from '@firebase/auth';
 import { NavController } from '@ionic/angular';
 import { AuthApi } from '../auth/auth.api';
-//import { RegisterRepository } from 'src/app/repository/register.repository';
-
-//import { getAuth } from 'firebase/auth';
+import { getAuth } from '@angular/fire/auth';
 
 @Injectable()
 export class BadgesApi {
   constructor(private Nav: NavController, private firestore: Firestore, private authApi: AuthApi) {}//, private readonly authObject: Auth, 
 
-//   badges$() {
-//     return authState(this.authObject);
-//   }
-badges$() {
-    const id = this.authApi.getCurrentUserId();
-    alert(id);
+  badges$() {
+    // const id = this.authApi.getCurrentUserId();
+    const auth = getAuth();
+    const currUserId = auth.currentUser?.uid;
+    // alert("IN badges.api.ts");
+    // alert(currUserId);
     const docRef = doc(
       this.firestore,
-      `badges/${id}`   //check how it is modelled in firebase
-    ).withConverter<IBadges>({       //convert our firestore data into INotificationBox type
+      `badges/${currUserId}`
+    ).withConverter<IBadges>({       //convert our firestore data into the IBadges type
       fromFirestore: (snapshot) => {
         return (snapshot.data() as IBadges);
       },
@@ -39,22 +29,4 @@ badges$() {
     });
     return docData(docRef, { idField: 'id' });
   }
-
-
-
-
-//   async login(regEmail: string, regPassword: string) {
-//     try {
-//       //alert("in auth api login function");
-//       await signInWithEmailAndPassword(this.authObject, regEmail, regPassword);
-//       //return await signInWithEmailAndPassword(this.authObject, regEmail, regPassword);
-      
-//       this.Nav.navigateRoot('/home'); // this is so they are only directed to login when they enter a valid email and password combination
-//     }catch (error) {
-//       // Handle the Firebase error
-//       console.error('Firebase error:', error);
-//       alert("Incorrect email password combination");
-//       //return "";
-//     }
-//   }
 }
--- src/app/states/badges/badges.state.ts
+++ src/app/states/badges/badges.state.ts
@@ -1,20 +1,12 @@
 import { Injectable } from '@angular/core';
 import { User } from '@angular/fire/auth';
-// import {
-//   getCurrentUserId,
-//   LoginAuth,
-//   Logout,
-//   RegisterAuth,
-//   SetUser,
-//   SubscribeToAuthState,
-// } from 'src/app/actions/auth';
-import { Action, Selector, State, StateContext } from '@ngxs/store';
+import { Action, Selector, State, StateContext, Store } from '@ngxs/store';
 import { Navigate } from '@ngxs/router-plugin';
 
 import { BadgesApi } from './badges.api';
 
 import produce from 'immer';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { IBadges } from 'src/app/models/badges.model';
 import { SetBadges, SubscribeToBadges } from 'src/app/actions/badges.actions';
 
@@ -33,18 +25,24 @@ export interface BadgesStateModel {
 })
 @Injectable()
 export class BadgesState {
-  constructor(private readonly badgesApi: BadgesApi) {}
+  constructor(private readonly badgesApi: BadgesApi, private readonly store: Store) { }
 
   @Selector()
   static receivedBadges(state: BadgesStateModel) {
     return state.currBadges?.receivedBadges;
   }
 
+  @Selector()
+  static currBadges(state: BadgesStateModel) {
+    return state.currBadges;
+  }
+
   @Action(SubscribeToBadges)
   public subscribeToBadgesState(context: StateContext<BadgesStateModel>) {
-    alert("IN subscribeToBadges");
     return this.badgesApi.badges$().pipe(
-      tap((currBadges: IBadges | null) => {
+      tap((currBadges: IBadges) => {
+        console.log("IN subscribeToBadges");
+        console.log(currBadges);
         context.dispatch(new SetBadges(currBadges));
       })
     );
@@ -52,43 +50,10 @@ export class BadgesState {
 
   @Action(SetBadges)
   async setBadges(context: StateContext<BadgesStateModel>, { badges }: SetBadges) {
-    context.setState(
+    return context.setState(
       produce((repr) => {
         repr.currBadges = badges;
       })
     );
   }
-
-//   @Action(RegisterAuth)
-//   async register(
-//     context: StateContext<AuthenticationStateModel>,
-//     { email, password }: RegisterAuth
-//   ) {
-//     try {
-//       // alert("auth.state.ts");
-//       // alert(email);
-//       // alert(password);
-//       await this.authenticationApi.register(email, password);
-//       return context.dispatch(new Navigate(['home']));
-//     } catch (err) {
-//       return (err as Error).message;
-//     }
-//   }
-
-//   @Action(LoginAuth)
-//   async login(context: StateContext<AuthenticationStateModel>, { email, password }: LoginAuth) {
-//     //alert("In auth state login function");
-//     try {
-//       await this.authenticationApi.login(email, password);
-//       return context.dispatch(new Navigate(['home']));
-//     } catch (err) {
-//       return context.dispatch(new Navigate(['/login'])); //maybe we should change it to register
-//     }
-//   }
-
-//   @Action(Logout)
-//   async logout(context: StateContext<AuthenticationStateModel>) {
-//     await this.authenticationApi.logout();
-//     return context.dispatch(new Navigate(['/login']));
-//   }
 }
