a9d21bcb679649be7db38bf2ccdd33a9f4809601
Shuaib Nuruddin <95973048+ShuaibNuruddin@users.noreply.github.com>
2023-06-12 23:58:04 +0200

    made popups

--- src/app/pages/points/points.page.ts
+++ src/app/pages/points/points.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-points',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PointsPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+
   }
 
+  async showWorkoutPlanPopup() {
+    const alert = await this.alertController.create({
+      header: 'Congratulations!',
+      message: 'You earned 50 points for completing your workout plan.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async showGymSchedulePopup() {
+    const alert = await this.alertController.create({
+      header: 'Congratulations!',
+      message: 'You earned 75 points for attending 3 workout sessions.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async showFitnessGoalPopup() {
+    const alert = await this.alertController.create({
+      header: 'Congratulations!',
+      message: 'You earned 100 points for completing your fitness goal.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
