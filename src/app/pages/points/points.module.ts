0665c11bfc03b496b224b37e41075828d4e57334
Shuaib Nuruddin <95973048+ShuaibNuruddin@users.noreply.github.com>
2023-06-12 16:33:29 +0200

    Part of points UI

--- src/app/pages/points/points.module.ts
+++ src/app/pages/points/points.module.ts
@@ -5,16 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { PointsPageRoutingModule } from './points-routing.module';
-
+import { PointsModule as PointsStatesModule } from 'src/app/states/points';
 import { PointsPage } from './points.page';
+import { PointsApi } from 'src/app/states/points/points.api';
+import { AuthApi } from 'src/app/states/auth/auth.api';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PointsPageRoutingModule
+    PointsPageRoutingModule,
+    PointsStatesModule
   ],
-  declarations: [PointsPage]
+  declarations: [PointsPage],
+  providers: [PointsApi, AuthApi]
 })
 export class PointsPageModule {}
