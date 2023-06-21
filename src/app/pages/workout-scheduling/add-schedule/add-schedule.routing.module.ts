9204e2ca513a6a67c03f4d233169e329fb23d988
Thabo Letsoalo <126817800+ThaboLetsoalo@users.noreply.github.com>
2023-06-21 21:34:14 +0200

    add schedule component modified

--- src/app/pages/workout-scheduling/add-schedule/add-schedule.routing.module.ts
+++ src/app/pages/workout-scheduling/add-schedule/add-schedule.routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {AddScheduleComponent } from './popout-add-schedule.component';
+import {AddScheduleComponent } from './add-schedule.component';
 
 const routes: Routes = [
   {
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class WorkoutSchedulingPageRoutingModule {}
+export class AddSchedulePageRoutingModule {}
