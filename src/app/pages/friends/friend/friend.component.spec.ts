877cbb6f257392301a249b179bddbc92b3eb50b1
Thabo Letsoalo <126817800+ThaboLetsoalo@users.noreply.github.com>
2023-06-26 13:17:57 +0200

    tests revert

--- src/app/pages/friends/friend/friend.component.spec.ts
+++ src/app/pages/friends/friend/friend.component.spec.ts
@@ -1,42 +1,22 @@
-import { NavController } from '@ionic/angular';
-import { Store } from '@ngxs/store';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
 import { FriendComponent } from './friend.component';
-import { StageOtheruserInfo, RemoveFriendAction } from 'src/app/actions';
-import { IFriendsModel } from 'src/app/models';
-
 describe('FriendComponent', () => {
-  let friendComponent: FriendComponent;
-  let navControllerMock: any;
-  let storeMock: any;
-  const friendMock: IFriendsModel = { userId: 'test_id', name: 'Friend 1' , profileURL: 'assets/Asset 2.png'};
+  let component: FriendComponent;
+  let fixture: ComponentFixture<FriendComponent>;
 
-  beforeEach(() => {
-    navControllerMock = {
-      navigateForward: jest.fn(),
-    };
-    storeMock = {
-      dispatch: jest.fn(),
-    };
-    friendComponent = new FriendComponent(navControllerMock, storeMock);
-  });
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FriendComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
 
-  describe('viewOtherUser', () => {
-    it('should dispatch StageOtheruserInfo action with the friend', () => {
-      friendComponent.friend = friendMock;
+    fixture = TestBed.createComponent(FriendComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
-      friendComponent.viewOtherUser();
-
-      expect(storeMock.dispatch).toHaveBeenCalledWith(new StageOtheruserInfo(friendMock));
-    });
-  });
-
-  describe('removeFriend', () => {
-    it('should dispatch RemoveFriendAction action with the friend', () => {
-      friendComponent.friend = friendMock;
-
-      friendComponent.removeFriend();
-
-      expect(storeMock.dispatch).toHaveBeenCalledWith(new RemoveFriendAction(friendMock));
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
