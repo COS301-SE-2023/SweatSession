13435d09280ca788b4c1a175138c27436be05277
Thabo Letsoalo <126817800+ThaboLetsoalo@users.noreply.github.com>
2023-06-26 13:08:10 +0200

    Revert "friends feature testing, not running properly"
    This reverts commit 0db73b8fdd76907f72a9bbe0f51b0b02de260cb1.

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,8 @@
 module.exports = {
-    // preset: 'jest-preset-angular',
-    // setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    // testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
-    // collectCoverage: true,
-    // coverageReporters: ['html', 'lcov', 'text-summary'],
-    // coverageDirectory: '<rootDir>/coverage',
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    globals: {
-      'ts-jest': {
-        tsconfig: './tsconfig.json',
-      },
-    },
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+    collectCoverage: true,
+    coverageReporters: ['html', 'lcov', 'text-summary'],
+    coverageDirectory: '<rootDir>/coverage',
   };
