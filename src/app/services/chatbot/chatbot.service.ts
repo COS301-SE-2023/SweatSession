4d2c2ef7a79970868337b1eca78cce390f2b550e
Daniel-Bezuidenhout <u21539686@tuks.co.za>
2023-07-03 21:26:58 +0200

    Chagned constructor and added sendMessage function to chatbot service

--- src/app/services/chatbot/chatbot.service.ts
+++ src/app/services/chatbot/chatbot.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatbotService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sendMessage(userMessage: string) {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${environment.openai_api_key}`);
+    const body = {
+        prompt: userMessage,
+        max_tokens: 60
+    };
+    return this.http.post(environment.openai_endpoint, body, { headers });
+}
 }
