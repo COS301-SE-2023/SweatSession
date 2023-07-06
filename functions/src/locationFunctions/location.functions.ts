8332c6321cebf61fac126af2e397a762aeba7f83
ShuaibNuruddin <u21497402@tuks.co.za>
2023-07-06 23:49:49 +0200

    updated cloud function

--- functions/src/locationFunctions/location.functions.ts
+++ functions/src/locationFunctions/location.functions.ts
@@ -5,10 +5,10 @@ export const proxyRequest = functions.https.onRequest((req, res) => {
     const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.query.latitude},${req.query.longitude}&radius=${req.query.radius}&type=gym&key=${req.query.key}`;
     
     axios.get(apiUrl)
-      .then(response => {
+      .then((response: { data: any; }) => {
         res.json(response.data);
       })
-      .catch(error => {
+      .catch((error: any) => {
         res.status(500).send('Error proxying the request');
       });
   });
