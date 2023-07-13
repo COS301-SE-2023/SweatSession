cb6139eb8744c7c7fbad5aa59321ebc0e3e78c33
ShuaibNuruddin <u21497402@tuks.co.za>
2023-07-13 13:07:34 +0200

    integrated pubsub funct into index.js

--- functions/index.js
+++ functions/index.js
@@ -19,39 +19,40 @@ exports.nearbyGymProxyRequest = functions.https.onRequest(async (req, res) => {
 
 // const firestore = admin.firestore();
 
-// export const checkScheduledWorkouts = functions.pubsub
-//   .schedule('every 1 minutes')
-//   .onRun(async (context) => {
-//     const Snapshot = await firestore.collection('WorkoutSchedule').get();
-//     Snapshot.forEach(async (doc)=>{
-//         const now = new Date();
-//         const scheduledWorkoutsRef = firestore.collection(`WorkoutSchedule/${doc.id}/userSchedules`);
-//         const snapshot = await scheduledWorkoutsRef.get();
+exports.checkScheduledWorkouts = functions.pubsub
+  .schedule('every 1 minutes')
+  .onRun(async (context) => {
+    const firestore = admin.firestore();
+    const Snapshot = await firestore.collection('WorkoutSchedule').get();
+    Snapshot.forEach(async (doc)=>{
+        const now = new Date();
+        const scheduledWorkoutsRef = firestore.collection(`WorkoutSchedule/${doc.id}/userSchedules`);
+        const snapshot = await scheduledWorkoutsRef.get();
 
-//         snapshot.forEach(async (innerDoc) => {
-//           const workout = innerDoc.data();
-//           const scheduledDateTime = workout.notifyAt;
+        snapshot.forEach(async (innerDoc) => {
+          const workout = innerDoc.data();
+          const scheduledDateTime = workout.notifyAt;
 
 
-//           if (scheduledDateTime <= now && workout.status !== 'completed' && !workout.notified) {
-//             console.log(workout.completeAt);
-//             console.log(now);
-//             Send a notification to the user userId == doc.id
-//             await innerDoc.ref.update({notified: true});
-//             await sendNotificationToUser(workout, "You have a session in 5 minutes");
+          if (scheduledDateTime <= now && workout.status !== 'completed' && !workout.notified) {
+            console.log(workout.completeAt);
+            console.log(now);
+            // Send a notification to the user userId == doc.id
+            await innerDoc.ref.update({notified: true});
+            await sendNotificationToUser(workout, "You have a session in 5 minutes");
 
-//             if(workout.completeAt <= now) {
-//               Send a notification to the user userId == doc.id
-//               await sendNotificationToUser(workout, "schedule completed");
-//               await innerDoc.ref.update({ status: 'completed' });
-//             }
-//           }
-//         });
-//     })
+            if(workout.completeAt <= now) {
+              // Send a notification to the user userId == doc.id
+              await sendNotificationToUser(workout, "schedule completed");
+              await innerDoc.ref.update({ status: 'completed' });
+            }
+          }
+        });
+    })
        
-//     return null;
-//   });
+    return null;
+  });
 
-// async function sendNotificationToUser(schedule, message) {
-//   //implement the notification logic here....
-// }
+async function sendNotificationToUser(schedule, message) {
+  //implement the notification logic here....
+}
