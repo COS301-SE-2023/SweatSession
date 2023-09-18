05cca9d0ee21da60e39693e44442a626350c2428
ShuaibNuruddin <u21497402@tuks.co.za>
2023-09-18 16:09:39 +0200

    gets users workout sessions

--- src/app/pages/calories-calculator/exercise-calculator/exercise-calculator.component.ts
+++ src/app/pages/calories-calculator/exercise-calculator/exercise-calculator.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { getAuth } from '@angular/fire/auth';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
-import { take } from 'rxjs';
+import { take, tap } from 'rxjs';
 import { Exercise } from 'src/app/models/exercise.model';
-import { ExerciseService } from 'src/app/services';
+import { ExerciseService, WorkoutscheduleService } from 'src/app/services';
 
 @Component({
   selector: 'exercise-calculator',
   templateUrl: './exercise-calculator.component.html',
   styleUrls: ['./exercise-calculator.component.scss'],
 })
-export class ExerciseCalculatorComponent  implements OnInit {
+export class ExerciseCalculatorComponent implements OnInit {
 
   workoutForm: FormGroup;
   exercisesArray: Exercise[] = [];
   deletedExercises: string[] = [];
   scheduleId: string;
+  currUserId: string | undefined;
+  plannedWorkouts: any;
 
   constructor(
     private exerciseService: ExerciseService,
-    private formBuilder: FormBuilder
-  ) { 
+    private formBuilder: FormBuilder,
+    private workoutscheduleService: WorkoutscheduleService
+  ) {
     this.workoutForm = this.formBuilder.group({
       exercises: this.formBuilder.array([])
     });
@@ -32,15 +36,16 @@ export class ExerciseCalculatorComponent  implements OnInit {
     //     id: exercise.id,
     //   }));
     //   console.log('Fetched exercises:', this.exercisesArray);
-      this.populateFormWithExercises();
+    this.populateFormWithExercises();
     // });
+    this.getUserWorkouts();
   }
 
-  deleteExercise(index: number){
+  deleteExercise(index: number) {
     this.exercises.removeAt(index);
   }
 
-  addExercise(){
+  addExercise() {
     const exerciseControl = this.formBuilder.group({
       name: [''],
       sets: [''],
@@ -65,5 +70,23 @@ export class ExerciseCalculatorComponent  implements OnInit {
       });
       exercisesFormArray.push(exerciseControl);
     });
+  }
+
+  async getUserWorkouts() {
+    const auth = getAuth();
+    this.currUserId = auth.currentUser?.uid;
+    if (this.currUserId != undefined) {
+      sessionStorage.setItem('currUserId', this.currUserId);
+    } else {
+      this.currUserId = sessionStorage.getItem('currUserId')!;
+    }
+    (await this.workoutscheduleService.getSchedules({ userId: this.currUserId })).subscribe(
+      (response) => {
+        // alert(response)
+        this.plannedWorkouts = response;
+        console.log("Planned workouts");
+        console.log(this.plannedWorkouts);
+      }
+    )
   }
 }
