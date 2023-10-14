5319793b9bda24bba90c28a0b9c36c6e1c51ad2e
LUQMAAN-ABRAM <104630753+LUQMAAN-ABRAM@users.noreply.github.com>
2023-10-15 00:16:11 +0200

    busy with reminder

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -3,10 +3,13 @@ import { Select, Store } from '@ngxs/store';
 import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
 import { Observable, tap } from 'rxjs';
 import { GetWorkoutSchedules } from 'src/app/actions';
-import { IWorkoutScheduleModel } from 'src/app/models';
+import { IGetWorkoutSchedules } from 'src/app/models';
+import { IGotWorkoutSchedules } from 'src/app/models';
 import { IBadges } from 'src/app/models/badges.model';
 import { PointsApi, WorkoutSchedulingState } from 'src/app/states';
 import { register } from 'swiper/element/bundle';
+import {WorkoutscheduleService} from 'src/app/services/workoutschedule/workoutschedule.service';
+import { getAuth } from 'firebase/auth';
 
 register();
 
@@ -18,10 +21,24 @@ register();
 export class DashboardPage implements OnInit {
   
   sessionsCompleted: Number = 0;
-  constructor(private store:Store, private pointsApi: PointsApi) { }
+  Flag: Boolean = false;
+  workoutlist: IGotWorkoutSchedules;
+  workoutlists: IGetWorkoutSchedules;
+  auth = getAuth();
+  currUserId = this.auth.currentUser?.uid;
+  constructor(private store:Store, private pointsApi: PointsApi, private workoutscheduleservice: WorkoutscheduleService ) { 
+    this.workoutlists = {
+      userId: this.currUserId! // Replace 'yourUserIdHere' with the actual user ID.
+    };
+  }
 
   ngOnInit(){
     this.getSessionsAttended()
+    if(!this.Flag){
+      this.getWorkoutScheduled(this.workoutlists);
+      this.Flag = true;
+
+    }
   }
 
   getSessionsAttended() {
@@ -31,4 +48,33 @@ export class DashboardPage implements OnInit {
       })
     ).subscribe();
   }
+
+  async getWorkoutScheduled(request: IGetWorkoutSchedules){
+  (await this.workoutscheduleservice.getSchedules(request)).subscribe((workouts: IGotWorkoutSchedules) => {
+    this.workoutlist = workouts;
+    for(let i=0 ; i<this.workoutlist.schedules.length; i++){
+      if(this.workoutlist.schedules[i].date == this.getMinDate()){
+        console.log("today date");
+      }
+
+    }
+    console.log(this.workoutlist);
+    console.log(this.getMinDate());
+  });
+}
+
+getMinDate(): Date {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth();
+  const currentDay = currentDate.getDate();
+
+  return new Date(currentYear, currentMonth, currentDay);
+}
+
+
+
+
+
+
 }
