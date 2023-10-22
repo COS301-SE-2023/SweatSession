ef72c26a004512594c65b34c7e15fa3c39a01162
Triumph Ndlovu <realtriumphndlovu@gmail.com>
2023-10-22 20:01:48 +0200

    calories tracking working

--- src/app/pages/healthdata/healthdata.page.ts
+++ src/app/pages/healthdata/healthdata.page.ts
@@ -19,7 +19,12 @@ export class HealthDataPage implements OnInit {
   isBeating: boolean = false;
 
   healthDataForm: FormGroup;
+  bmr: number;
+  BMIValue: number;
   isLoading: boolean = true;
+  commitmentLevel: number;
+  TDEEValue: number;
+  targetCalories: number;
   private ngUnsubscribe = new Subject();
   
   startBeating() {
@@ -134,8 +139,130 @@ export class HealthDataPage implements OnInit {
       await this.notifyService.presentSuccessToast('Health data added Successfully.');
     }
 
-    // const cal = this.CalorieTargetComponent.calculateandGet_BMR();
-    this.healthDataService.addWeightData(this.healthDataForm.value.weight, this.currUserId);
+    this.calculateandGet_BMR()
+    const TARGETC = this.calculateTargetCalories();
+    console.log("target is => " + TARGETC);
+    this.healthDataService.addWeightData(this.healthDataForm.value.weight,TARGETC, this.currUserId);
 
   }
+
+  calculateandGet_BMR() { //using  the Revised Harris-Benedict Equation
+
+    let height = 0;
+    let weight = 0;
+    let age = 0;
+    let gender = "none";
+
+    const auth = getAuth();
+    this.currUserId = auth.currentUser?.uid;
+
+    if (this.currUserId != undefined) {
+        sessionStorage.setItem('currUserId', this.currUserId);
+    } else {
+        this.currUserId = sessionStorage.getItem('currUserId') ?? "";
+    }
+
+  
+  
+
+                height = this.healthDataForm.value.height;
+                weight = this.healthDataForm.value.weight;
+                age = this.healthDataForm.value.age;
+                gender = this.healthDataForm.value.gender;
+
+
+                let bmr = 0;
+
+                if (gender == "Male") {
+                    let step1 = 13.397 * weight;
+                    let step2 = 4.799 * height;
+                    let step3 = 5.677 * age;
+                    let step4 = 88.362;
+                    bmr = step1 + step2 - step3 + step4;
+                } else {
+                    let step1 = 9.247 * weight;
+                    let step2 = 3.098 * height;
+                    let step3 = 4.330 * age;
+                    let step4 = 447.593;
+                    bmr = step1 + step2 - step3 + step4;
+                }
+                this.bmr = bmr;
+
+
+                //calculate BMI
+                let BMI = 0;
+                let heightM = height / 100;
+                BMI = weight / (heightM * heightM);
+                this.BMIValue = BMI;
+
+                //calculate TDEE
+                this.calculateandGet_TDEE();
+                this.calculateTargetCalories();
+    }
+
+    calculateandGet_TDEE() {
+
+      let TDEE = 0;
+
+      if (this.healthDataForm.value.workoutCommitment == "Low Commitment (0-1 days/week)") {
+          this.commitmentLevel = 1.2;
+      } else if (this.healthDataForm.value.workoutCommitment == "Medium Commitment (2-3 days/week)") {
+          this.commitmentLevel = 1.375;
+      } else if (this.healthDataForm.value.workoutCommitment == "High Commitment (4-5 days/week)") {
+          this.commitmentLevel = 1.55;
+      } else if (this.healthDataForm.value.workoutCommitment == "Extreme Commitment (6+ days/week)") {
+          this.commitmentLevel = 1.725;
+      }
+
+      console.log("The value: " + this.commitmentLevel);
+
+      TDEE = this.bmr * this.commitmentLevel;
+
+      this.TDEEValue = TDEE;
+      return TDEE;
+  }
+
+  calculateTargetCalories(): number {
+
+    console.log("The Weight: " + this.healthDataForm.value.weightGoals);
+    console.log("The TDEEE: " + this.TDEEValue);
+    let targetCalories = this.TDEEValue;
+
+    if (this.healthDataForm.value.weightGoals == 0) {
+        this.targetCalories = targetCalories;
+    } else if (this.healthDataForm.value.weightGoals > 0) {
+        if (this.healthDataForm.value.weightGoals == 1) {
+            targetCalories = targetCalories + 1100;
+            this.targetCalories = targetCalories;
+        } else if (this.healthDataForm.value.weightGoals == 0.75) {
+            targetCalories = targetCalories + 825;
+            this.targetCalories = targetCalories;
+        } else if (this.healthDataForm.value.weightGoals == 0.5) {
+            targetCalories = targetCalories + 550;
+            this.targetCalories = targetCalories;
+        } else if (this.healthDataForm.value.weightGoals == 0.25) {
+            targetCalories = targetCalories + 275;
+            this.targetCalories = targetCalories;
+        }
+
+    } else if (this.healthDataForm.value.weightGoals < 0) {
+        if (this.healthDataForm.value.weightGoals == -1) {
+            targetCalories = targetCalories - 1100;
+            this.targetCalories = targetCalories;
+        } else if (this.healthDataForm.value.weightGoals == -0.75) {
+            targetCalories = targetCalories - 825;
+            this.targetCalories = targetCalories;
+        } else if (this.healthDataForm.value.weightGoals == -0.5) {
+            targetCalories = targetCalories - 550;
+            this.targetCalories = targetCalories;
+        } else if (this.healthDataForm.value.weightGoals == -0.25) {
+            targetCalories = targetCalories - 275;
+            this.targetCalories = targetCalories;
+        }
+    }
+
+
+    return targetCalories;
+}
+
 }
